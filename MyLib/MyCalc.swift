556b81d978b4985e52f5e598a712a7283fb79d1e
Serhii Yefanov <serhii.yefanov@fivesysdev.com>
2021-10-15 11:53:34 +0300

    Initial

---
+++ MyLib/MyCalc.swift
@@ -0,0 +1,14 @@
+//
+//  MyCalc.swift
+//  MyLib
+//
+//  Created by Serhii Yefanov on 15.10.2021.
+//
+
+import Foundation
+
+public class MyCalculator {
+    public static func add(_ first: Int, _ second: Int) -> Int {
+        return first + second
+    }
+}
